8d13479f5c510371d2d9779b4680ba75f0f70c33
galmiron86 <almirongerardo86@gmail.com>
2023-10-15 20:50:10 -0300

    ulti

---
+++ tpo-one-page-Seccion-Gera-master/js/api_consumo.js
@@ -0,0 +1,15 @@
+const apiKey = 'b10dead2496814f4480c0df14eae72d0';
+const ciudad = 'buenos_aires';
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}`)
+  .then(response => response.json())
+  .then(data => {
+    const climaElement = document.getElementById('clima');
+    const temperaturaCelsius = data.main.temp - 273.15; // Conversión a grados Celsius
+
+    climaElement.innerHTML = `
+      <h2>${data.name}</h2>
+      <p>Temperatura: ${temperaturaCelsius.toFixed(2)} °C</p>
+    `;
+  })
+  .catch(error => console.error('Error:', error));
